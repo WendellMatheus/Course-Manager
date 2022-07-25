25be1847eec7ea7f5685edea246487801743e840
WendellMatheus <wendellmatheus77@gmail.com>
2022-07-25 13:49:06 -0300

    uploading application with https request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,26 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './components/courses/course-list.component';
-import { StarComponent } from './components/star/star.component';
-import { ReplacePipe } from './components/pipe/replace.pipe';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { Error404Component } from './components/error-404/error-404.component';
-import { CourseInfoComponent } from './components/courses/course-info.component';
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
-    NavBarComponent,
-    Error404Component,
-    CourseInfoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
     HttpClientModule,
+    CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
-      {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }
     ])
   ],
